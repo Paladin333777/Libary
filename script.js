9780a63b87868ac8fa76cebf553600060abd949d
Paladin333777 <romavolvyn@gmail.com>
2023-08-12 15:34:03 -0400

    Add basic layout and modal.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let mylibary = [];
+
+for(let i = 0; i < mylibary.length; i++) {
+    let currBook = myLibary[i];
+
+}
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -7,4 +14,27 @@ function Book(title, author, pages, read){
 
 const book1 = new Book("The Last Wish", "Andrzej Sapkowski", "352", "No")
 
-console.log(book1)
+Book.prototype.myLove = function() {
+    console.log("I love The Witcher Universe.")
+}
+
+console.log(book1.valueOf())
+
+const modal = document.getElementById("modal")
+const newBook = document.getElementById("newbook")
+
+newBook.addEventListener("click", () => {
+    modal.showModal();
+})
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      modal.close()
+    }
+  })
