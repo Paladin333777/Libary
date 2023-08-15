0efe3721dc293667b1e11447caad8c510d23128c
Paladin333777 <romavolvyn@gmail.com>
2023-08-15 01:08:30 -0400

    Work on JS.

--- script.js
+++ script.js
@@ -3,26 +3,39 @@ const titleH  = document.getElementById('title')
 const pagesH  = document.getElementById("pages")
 const readH = document.getElementById('read')
 const submit = document.querySelector(".submit-btn")
+const form = document.getElementById('book-form')
+const books = document.querySelector(".books")
 
 const myLibary = [];
-
-// for(let i = 0; i < myLibary.length; i++) {
-//     let currBook = myLibary[i];
-// }
+const workLibary = [];
 
 
 function Book(title, author, pages, read){
-    this.title = titleH.value;
-    this.author = authorH.value;
-    this.pages = pagesH.value;
-    this.read = readH.value;
-    myLibary.push(this)
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  myLibary.push(this)
+}
+
+function addBook() {
+for(let i = 0; i < myLibary.length; i++) {
+    let currBook = myLibary[i];
+    const book = document.createElement('div')
+    book.classList.add('book')
+    book.innerHTML = `Author: ${currBook.author}<br>Tittle: ${currBook.title}<br>Pages: ${currBook.pages}<br>Read: ${currBook.read}`
+    workLibary.push(book++)
+    books.appendChild(book)
+}
 }
 
 submit.addEventListener('click' , (event) => {
     event.preventDefault();
-    let book1 = new Book(titleH.value, authorH.value, pagesH.value, readH.value)
+    let bookN = titleH.value
+    bookN = new Book(titleH.value, authorH.value, pagesH.value, readH.value)
+    addBook()
     modal.close()
+    form.reset()
 })
 
 
